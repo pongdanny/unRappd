13d0203faa768a13feca531f43646aa64259cf8e
pongdanny <pongdaniel17@gmail.com>
2021-01-05 12:58:18 -0500

    also added seed data

--- python-project-starter 2/app/api/user_routes.py
+++ python-project-starter 2/app/api/user_routes.py
@@ -18,11 +18,3 @@ def user(id):
     user = User.query.get(id)
     return user.to_dict()
 
-@user_routes.route("/<userId>/artists/<artistId>")
-def user_items(userId, itemId):
-    items = db.session.query(Item).filter_by(
-        sellerId=userId).filter(Item.id != itemId).all()
-
-    user_items = [item.to_dict() for item in items]
-
-    return jsonify(user_items)
