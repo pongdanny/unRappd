b4116a8ad236f8af098b69bbe3a5701d4e4d4569
pongdanny <pongdaniel17@gmail.com>
2021-01-08 13:14:49 -0500

    artist route syntax fix

--- python-project-starter 2/app/api/artist_routes.py
+++ python-project-starter 2/app/api/artist_routes.py
@@ -9,23 +9,7 @@ artist_routes = Blueprint('artists', __name__)
 @artist_routes.route('/', methods=["GET"])
 def get_all_artist():
     artists = Artist.query.all()
-    return {"artists": [artist.to_dict() for artist in artists]}
-
-
-# @artist_routes.route('/<int:id>', methods=["GET"])
-# def one_artist(id):
-#     artist = Artist.query.get(id)
-#     return artist.to_dict()
-
-
-# @artist_routes.route('/<int:id>', methods=["DELETE"])
-# def delete_artist(id):
-#     artist_to_delete = Artist.query.get(id)
-#     if artist_to_delete:
-#         artist_to_delete.delete()
-#         return {"response": [f"Artist has been deleted!"]}
-#     else:
-#         return {"errors": [f"Artist does not exist!"]}
+    return {"artists": [artis.to_dict() for artist in artists]}
 
 # @artist_routes.route("/", methods=["GET"])
 # def artists():
@@ -41,6 +25,19 @@ def get_all_artist():
 # def artist(id):
 #     artist = db.session.query(Artist).get(id)
 
-#     return jsonify(artists.to_dict())
+# @artist_routes.route('/<int:id>', methods=["GET"])
+# def one_artist(id):
+#     artist = Artist.query.get(id)
+#     return artist.to_dict()
+
+
+# @artist_routes.route('/<int:id>', methods=["DELETE"])
+# def delete_artist(id):
+#     artist_to_delete = Artist.query.get(id)
+#     if artist_to_delete:
+#         artist_to_delete.delete()
+#         return {"response": [f"Artist has been deleted!"]}
+#     else:
+#         return {"errors": [f"Artist does not exist!"]}
 
 
