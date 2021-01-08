18726b3f1ff5129861785e7fd9e827c966f36262
pongdanny <pongdaniel17@gmail.com>
2021-01-08 13:09:40 -0500

    trying to setup song route

--- python-project-starter 2/app/api/artist_routes.py
+++ python-project-starter 2/app/api/artist_routes.py
@@ -1,25 +1,46 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import artist, db
+from app.models import Artist, db
 from sqlalchemy.orm import selectinload
 
 
 artist_routes = Blueprint('artists', __name__)
 
 
-@artist_routes.route("/", methods=["GET"])
-def artists():
-    artists = db.session.query(Artist).options(selectinload(Artist.user)).all()
-
-    artists_with_seller = [{**artist.to_dict(), "user": artist.user.to_dict()}
-                        for artist in artists]
-
-    return jsonify(artists_with_user)
+@artist_routes.route('/', methods=["GET"])
+def get_all_artist():
+    artists = Artist.query.all()
+    return {"artists": [artist.to_dict() for artist in artists]}
 
 
-@artist_routes.route("/<id>")
-def artist(id):
-    artist = db.session.query(Artist).get(id)
+# @artist_routes.route('/<int:id>', methods=["GET"])
+# def one_artist(id):
+#     artist = Artist.query.get(id)
+#     return artist.to_dict()
 
-    return jsonify(artist.to_dict())
+
+# @artist_routes.route('/<int:id>', methods=["DELETE"])
+# def delete_artist(id):
+#     artist_to_delete = Artist.query.get(id)
+#     if artist_to_delete:
+#         artist_to_delete.delete()
+#         return {"response": [f"Artist has been deleted!"]}
+#     else:
+#         return {"errors": [f"Artist does not exist!"]}
+
+# @artist_routes.route("/", methods=["GET"])
+# def artists():
+#     artists = db.session.query(Artist).options(selectinload(Artist.user)).all()
+
+#     artists_with_user = [{**artists.to_dict(), "user": artists.user.to_dict()}
+#                         for artist in artists]
+
+#     return jsonify(artists_with_user)
+
+
+# @artist_routes.route("/<id>")
+# def artist(id):
+#     artist = db.session.query(Artist).get(id)
+
+#     return jsonify(artists.to_dict())
 
 
--- python-project-starter 2/app/api/song_routes.py
+++ python-project-starter 2/app/api/song_routes.py
@@ -1,25 +1,45 @@
-# from flask import Blueprint, jsonify, session, request
-# from app.models import artist, db
-# from sqlalchemy.orm import selectinload
+from flask import Blueprint, jsonify, session, request
+from app.models import Song, db
+from sqlalchemy.orm import selectinload
 
 
-# artist_routes = Blueprint('artists', __name__)
+song_routes = Blueprint('songs', __name__)
+
+@song_routes.route('/', methods=["GET"])
+def get_all_song():
+    songs = Song.query.all()
+    return {"songs": [song.to_dict() for song in songs]}
 
 
-# @artist_routes.route("/", methods=["GET"])
-# def artists():
-#     artists = db.session.query(Artist).options(selectinload(Artist.user)).all()
-
-#     artists_with_seller = [{**artist.to_dict(), "user": artist.user.to_dict()}
-#                         for artist in artists]
-
-#     return jsonify(artists_with_user)
+@song_routes.route('/<int:id>', methods=["GET"])
+def one_song(id):
+    song = Song.query.get(id)
+    return song.to_dict()
 
 
-# @artist_routes.route("/<id>")
-# def artist(id):
-#     artist = db.session.query(Artist).get(id)
+@song_routes.route('/<int:id>', methods=["DELETE"])
+def delete_song(id):
+    song_to_delete = Song.query.get(id)
+    if song_to_delete:
+        song_to_delete.delete()
+        return {"response": [f"Song has been deleted!"]}
+    else:
+        return {"errors": [f"Song does not exist!"]}
 
-#     return jsonify(artist.to_dict())
+# @song_routes.route("/", methods=["GET"])
+# def songs():
+#     songs = db.session.query(Song).options(selectinload(Song.user)).all()
+
+#     songs_with_artist = [{**song.to_dict(), "song": artist.song.to_dict()}
+#                         for song in songs]
+
+#     return jsonify(songs_with_artist)
+
+
+# @song_routes.route("/<id>")
+# def song(id):
+#     song = db.session.query(Song).get(id)
+
+#     return jsonify(song.to_dict())
 
 
