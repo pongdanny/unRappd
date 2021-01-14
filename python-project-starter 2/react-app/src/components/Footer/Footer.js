1f03a015662a59647091d3c31d51556e2bd92929
pongdanny <pongdaniel17@gmail.com>
2021-01-14 02:28:23 -0500

    added images for links

--- python-project-starter 2/react-app/src/components/Footer/Footer.js
+++ python-project-starter 2/react-app/src/components/Footer/Footer.js
@@ -4,15 +4,13 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="title">
-        UNRAPPD
-        <div className="divider"></div>
-      </div>
+      <div className="title">UNRAPPD</div>
       <div className="about">
         <a href="https://untappd.com">Inspired by UNTAPPD!</a>
       </div>
       <div className="links">
         <a
+          className="repo"
           href="https://github.com/pongdanny/unRappd"
           target="_blank"
           rel="noopener noreferrer"
@@ -24,21 +22,32 @@ const Footer = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Github
+          <img
+            className="githublink"
+            src="https://joshuapenalba.files.wordpress.com/2014/12/github-icon.png"
+          ></img>
         </a>
         <a
           href="https://www.linkedin.com/in/danielpong/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          LinkedIn
+          {" "}
+          <img
+            className="linkedinlink"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnabnti9oG9rBHosrWirRwdgeiSTzsnLkmZw&usqp=CAU"
+          ></img>
         </a>
         <a
           href="https://angel.co/u/dannypong"
           target="_blank"
           rel="noopener noreferrer"
         >
-          AngelList
+          {" "}
+          <img
+            className="angellistlink"
+            src="https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_ab2e1b537cf3eb902be59dba210c1f94/angellist.jpg"
+          ></img>
         </a>
       </div>
       <div className=""></div>
